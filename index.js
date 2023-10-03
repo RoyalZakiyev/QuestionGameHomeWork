ab93bffc9223e88ccde6bf98a54c504020397c80
RoyalZakiyev <zekiyevroyal5@gmail.com>
2023-10-03 19:36:14 +0400

    yeni proyekt

---
+++ index.js
@@ -0,0 +1,38 @@
+var q1 = prompt(
+  "1922-ci ildə 14 yaşında Bakı Dövlət Universitetinə daxil olan bu şəxs eyni vaxtda iki fakültədə: fizika-riyaziyyat və kimya fakültələrində təhsil alırdı"
+);
+
+if (q1 == "Lev Landau") {
+  console.log("Afərin növbəti mərhələyə keçdiniz");
+  alert("dogrudur");
+} else {
+  console.log("cavab səhvdir");
+  alert("yanlış cavab");
+}
+
+var q2 = prompt(
+  "Mirzə Ələkbər Sabirin şeirlər toplusu bu ad altında çap olunmuşdur?"
+);
+
+if (q2 == "Hophop-namə") {
+  console.log("Hophop-namə");
+  alert("Hophop-namə");
+} else {
+  console.log("yanlışdır");
+}
+
+var q3 = prompt("Fəzlullah Nəiminin son əsəri Səfərnamədir?");
+if (q3 == false) {
+  console.log("Vəsiyyətnamə");
+  alert("Vəsiyyətnamə");
+} else {
+  console.log("Yanlışdır");
+  alert("Yanlışdır");
+}
+
+var q4 = prompt("Ekliptikanın Cənub qütbü bu bürcdə yerləşir");
+console.log("Qızıl balıq");
+alert("Qızıl balıq");
+
+if (" ") {
+}
